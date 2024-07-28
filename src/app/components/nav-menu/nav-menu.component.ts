a433f362a45183864330c99fa4c0b2bd2068e984
CaptainCompileon <samko.adam0@gmail.com>
2024-07-28 10:36:33 +0200

    Create machine-service and use it in the nav-menu

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { LineMachineService } from '../../services/line-machine.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
     standalone: true,
-    imports: [],
+    imports: [AsyncPipe],
     selector: 'nav-menu',
     templateUrl: 'nav-menu.component.html',
-    styleUrl: './nav-menu.component.scss'
+    styleUrl: './nav-menu.component.scss',
 })
-
 export class NavMenuComponent implements OnInit {
-    constructor() { }
+    constructor(protected readonly lineMachineService: LineMachineService) {}
 
-    ngOnInit() { }
+    ngOnInit() {}
 }
