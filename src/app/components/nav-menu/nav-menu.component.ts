9f3ddfd0457fa49b7909771fe2622ae626b1fb97
CaptainCompileon <samko.adam0@gmail.com>
2024-07-28 11:32:21 +0200

    create simple nav-menu

--- src/app/components/nav-menu/nav-menu.component.ts
+++ src/app/components/nav-menu/nav-menu.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { LineMachineService } from '../../services/line-machine.service';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
 
 @Component({
     standalone: true,
-    imports: [AsyncPipe],
+    imports: [AsyncPipe, DatePipe],
     selector: 'nav-menu',
     templateUrl: 'nav-menu.component.html',
     styleUrl: './nav-menu.component.scss',
 })
 export class NavMenuComponent implements OnInit {
+    protected currentDateTime!: Date;
+    protected readonly userName: string = 'John Doe'; // Replace with actual user name
+
     constructor(protected readonly lineMachineService: LineMachineService) {}
 
-    ngOnInit() {}
+    ngOnInit(): void {
+        this.updateDateTime();
+        setInterval(() => this.updateDateTime(), 1000);
+    }
+
+    updateDateTime() {
+        this.currentDateTime = new Date();
+    }
 }
